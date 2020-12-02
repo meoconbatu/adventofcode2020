253ce2c024f0a091a6178c630d5281c16443315b
Thanh Mi Nguyen Thanh <thanhmi1111@live.com>
2020-12-02 12:13:38 +0800

    add command-line flags

--- main.go
+++ main.go
@@ -1,6 +1,19 @@
 package main
 
+import "flag"
+
 func main() {
-	day1Part1()
-	day1Part2()
+	dayth := flag.Int("day", 1, "day of puzzle")
+	part := flag.Int("part", 1, "1 or 2")
+
+	flag.Parse()
+
+	switch *dayth {
+	case 1:
+		if *part == 1 {
+			day1Part1()
+		} else {
+			day1Part2()
+		}
+	}
 }
