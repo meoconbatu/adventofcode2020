dfa37c56cd3ef934d7bd3acd1fa93190ae693ee1
Thanh Mi Nguyen Thanh <thanhmi1111@live.com>
2020-12-07 21:35:50 +0800

    use funcMap to call dynamic func

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"meoconbatu/adventofcode2020/day1"
 	"meoconbatu/adventofcode2020/day2"
 	"meoconbatu/adventofcode2020/day3"
@@ -10,49 +11,27 @@ import (
 	"meoconbatu/adventofcode2020/day6"
 )
 
+var funcMap map[string]interface{}
+
+func init() {
+	funcMap = map[string]interface{}{
+		"011": day1.Part1, "012": day1.Part2,
+		"021": day2.Part1, "022": day2.Part2,
+		"031": day3.Part1, "032": day3.Part2,
+		"041": day4.Part1, "042": day4.Part2,
+		"051": day5.Part1, "052": day5.Part2,
+		"061": day6.Part1, "062": day6.Part2,
+	}
+}
 func main() {
 	dayth := flag.Int("day", 1, "day of puzzle")
 	part := flag.Int("part", 1, "1 or 2")
 
 	flag.Parse()
 
-	switch *dayth {
-	case 1:
-		if *part == 1 {
-			day1.Part1()
-		} else {
-			day1.Part2()
-		}
-	case 2:
-		if *part == 1 {
-			day2.Part1()
-		} else {
-			day2.Part2()
-		}
-	case 3:
-		if *part == 1 {
-			day3.Part1()
-		} else {
-			day3.Part2()
-		}
-	case 4:
-		if *part == 1 {
-			day4.Part1()
-		} else {
-			day4.Part2()
-		}
-	case 5:
-		if *part == 1 {
-			day5.Part1()
-		} else {
-			day5.Part2()
-		}
-	case 6:
-		if *part == 1 {
-			day6.Part1()
-		} else {
-			day6.Part2()
-		}
-	}
-
+	execute(*dayth, *part)
+}
+func execute(dayth, part int) {
+	key := fmt.Sprintf("%02d%d", dayth, part)
+	funcMap[key].(func())()
 }
