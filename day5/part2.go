10262a3349f9a69b07b572453627e0bfb4aa9c66
Thanh Mi Nguyen Thanh <thanhmi1111@live.com>
2020-12-10 18:18:36 +0800

    add one more solution: xor

--- day5/part2.go
+++ day5/part2.go
@@ -2,6 +2,7 @@ package day5
 
 import (
 	"fmt"
+	"meoconbatu/adventofcode2020/utils"
 	"sort"
 )
 
@@ -15,6 +16,8 @@ func Part2() {
 func part2Core(ins []string) int {
 	return getMissingSeatID(ins)
 }
+
+// getMissingSeatID func O(nlogn)
 func getMissingSeatID(boardingPasses []string) int {
 	seatIDs := getListSeatID(boardingPasses)
 	sort.Ints(seatIDs)
@@ -25,6 +28,22 @@ func getMissingSeatID(boardingPasses []string) int {
 		}
 	}
 	return 0
+}
+
+// getMissingSeatID2 func O(n)
+func getMissingSeatID2(boardingPasses []string) int {
+	seatIDs := getListSeatID(boardingPasses)
+	min, max := utils.FindMinMax(seatIDs)
+
+	xor := 0
+	for i := min; i <= max; i++ {
+		xor ^= i
+	}
+
+	for i := 0; i < len(seatIDs); i++ {
+		xor ^= seatIDs[i]
+	}
+	return xor
 }
 func getListSeatID(boardingPasses []string) []int {
 	seatIDs := make([]int, len(boardingPasses))
